6cd963366d1fc375f2960fe197711a6ce5764b0f
Caleb Einarr <earthctzn1@gmail.com>
2019-04-08 20:20:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(c)
+    new_a = []
+    count = 0 
+     if collection[counter] % 2 == 0
+				yield collection[counter]
+				tmp_array << collection[counter]
+				counter += 1
+      else
+    count += 1
+      end
+    return new_a
+  end
 end
